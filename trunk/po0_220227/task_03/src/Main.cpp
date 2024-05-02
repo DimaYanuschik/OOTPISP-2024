da65ae05449c8f458a80ce4df5823ccbb18ef682
DimaYanuschik <kreot1n@yandex.ru>
2024-05-02 11:21:06 +0300

    smell fix

--- trunk/po0_220227/task_03/src/Main.cpp
+++ trunk/po0_220227/task_03/src/Main.cpp
@@ -41,8 +41,8 @@ int main()
 	numbers.Add(&real1);
 	numbers.Add(&real2);
 
-	Complex complex1((5.6f, 10.8f);
-	Complex complex2(17.9f, 11.56f);
+	Complex complex1((5.6, 10.8);
+	Complex complex2(17.9, 11.56);
 
 	if (complex1 == complex2)
 	{
