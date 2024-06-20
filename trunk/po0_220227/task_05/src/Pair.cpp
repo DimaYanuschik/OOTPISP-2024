ed1d3da6499bcbcd6f1bb2070e9afbc5a9267acc
DimaYanuschik <kreot1n@yandex.ru>
2024-06-20 20:40:53 +0300

    smell fix

--- trunk/po0_220227/task_05/src/Pair.cpp
+++ trunk/po0_220227/task_05/src/Pair.cpp
@@ -4,8 +4,3 @@ Pair::Pair(const int fNum, const double sNum)
     : firstNumber(fNum), secondNumber(sNum)
 {
 }
-
-bool Pair::operator==(const Pair& a) const
-{
-    return (firstNumber == a.firstNumber && secondNumber == a.secondNumber);
-}
